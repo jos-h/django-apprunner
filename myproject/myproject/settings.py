a0a5a405d15be9201b6abf16a50a360cce06622b
jos-h <kunal123jamdade@gmail.com>
2024-03-28 15:47:01 +0530

    updated runner file

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import json
+import dj_database_url
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,12 +68,20 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "DATABASE_SECRET" in environ:
+    database_secret = environ.get("DATABASE_SECRET")
+    db_url = json.loads(database_secret)["DATABASE_URL"] # type: ignore
+    DATABASES = {"default": dj_database_url.parse(db_url)}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "django",
+            "USER": "postgres",
+            "PASSWORD": "test",
+            "HOST": "localhost",
+        }
     }
-}
 
 
 # Password validation
